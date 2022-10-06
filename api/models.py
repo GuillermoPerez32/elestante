ace80faa780b523867b4ef1e3ba71434c72caf45
Luis Guillermo Pérez Rodríguez <86862509+GuillermoPerez32@users.noreply.github.com>
2022-10-05 22:30:10 -0600

    adicionado modelo para libros

--- api/models.py
+++ api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.urls import reverse
+from django.contrib.
 # Create your models here.
+
+
+class Libro(models.Model):
+
+    cantidad_unidades = models.IntegerField(_("cantidad de unidades"))
+    fecha = models.DateField(_("fecha"), auto_now=True)
+    usuario = models.ForeignKey("directorio.User", verbose_name=_(
+        "usuario"), on_delete=models.SET_NULL, blank=True)
+
+    class Meta:
+        verbose_name = _("libro")
+        verbose_name_plural = _("libros")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("libro_detail", kwargs={"pk": self.pk})
